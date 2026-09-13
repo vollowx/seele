0e52f70d750123185fd263eaec0eb5e85eb9eb1e
Vollow <vollowx@outlook.com>
2026-09-13 19:05:24 +0800

    Revert "feat(popoverController): remove (#14)"
    This reverts commit e0e5cc1beaa5a273dff9a6092b41c2b5bf714c88.

--- src/m3/tooltip/tooltip.ts
+++ src/m3/tooltip/tooltip.ts
@@ -5,9 +5,14 @@ import { tooltipStyles } from './tooltip-styles.css.js';
 
 /**
  * @tag md-tooltip
+ *
+ * TODO: should not disable SSR, since the raw text would flash, but could try
+ * to SSR a simpler one
  */
 @customElement('md-tooltip', true)
 export class M3Tooltip extends Tooltip {
+  override readonly _durations = { show: 150, hide: 150 };
+
   static override styles = [tooltipStyles];
 }
 
